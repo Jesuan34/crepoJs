c9b2749f5e97fbcedbdb5630b833fb75e0338557
Jesuan Rosin <jesuan34@gmail.com>
2022-02-17 17:46:50 -0300

    nuevo repo de JS

---
+++ JS/script.js
@@ -0,0 +1,91 @@
+
+alert("Averigue que linea de pesca necesita para su caña de flyfishing");
+
+let numeroCaña = parseInt(prompt("Ingrese el numero de su caña (1-15) o diga quiero aprender mas"));
+
+while (numeroCaña === "esc") {
+
+    switch (numeroCaña) {
+        case 1:
+            alert("Usted necesita una linea de 60 grains");
+            alert("Le gusta la pesca fina ;-)");
+            alert("Ven a disfrutar a https://proyecto-outdoor.vercel.app/");
+            break;
+        case 2:
+            alert("Usted necesita una linea de 80 grains");
+            alert("Le gusta la pesca fina ;-)");
+            alert("Ven a disfrutar a https://proyecto-outdoor.vercel.app/");
+            break;
+        case 3:
+            alert("Usted necesita una linea de 100 grains");
+            alert("Le gusta la pesca fina ;-)");
+            alert("Ven a disfrutar a https://proyecto-outdoor.vercel.app/");
+            break;
+        case 4:
+            alert("Usted necesita una linea de 120 grains");
+            alert("Le gusta la pesca fina ;-)");
+            alert("Ven a disfrutar a https://proyecto-outdoor.vercel.app/");
+            break;
+        case 5:
+            alert("Usted necesita una linea de 140 grains");
+            alert("Le gusta la pesca fina ;-)");
+            alert("Ven a disfrutar a https://proyecto-outdoor.vercel.app/");
+            break;
+        case 6:
+            alert("Usted necesita una linea de 160 grains");
+            alert("Usted es todo terreno ;-)");
+            alert("Ven a disfrutar a https://proyecto-outdoor.vercel.app/");
+            break;
+        case 7:
+            alert("Usted necesita una linea de 186 grains");
+            alert("Usted es todo terreno ;-)");
+            alert("Ven a disfrutar a https://proyecto-outdoor.vercel.app/");
+            break;
+        case 8:
+            alert("Usted necesita una linea de 210 grains");
+            alert("Usted es todo terreno ;-)");
+            alert("Ven a disfrutar a https://proyecto-outdoor.vercel.app/");
+            break;
+        case 9:
+            alert("Usted necesita una linea de 240 grains");
+            alert("Le gusta ir a lo grande ;-)");
+            alert("Ven a disfrutar a https://proyecto-outdoor.vercel.app/");
+            break;
+        case 10:
+            alert("Usted necesita una linea de 280 grains");
+            alert("Le gusta ir a lo grande ;-)");
+            alert("Ven a disfrutar a https://proyecto-outdoor.vercel.app/");
+            break;
+        case 11:
+            alert("Usted necesita una linea de 330 grains");
+            alert("Le gusta ir a lo grande ;-)");
+            alert("Ven a disfrutar a https://proyecto-outdoor.vercel.app/");
+            break;
+        case 12:
+            alert("Usted necesita una linea de 380 grains");
+            alert("Le gusta ir a lo grande ;-)");
+            alert("Ven a disfrutar a https://proyecto-outdoor.vercel.app/");
+            break;
+        case 13:
+            alert("Usted necesita una linea de 450 grains");
+            alert("Le gusta ir a lo grande ;-)");
+            alert("Ven a disfrutar a https://proyecto-outdoor.vercel.app/");
+            break;
+        case 14:
+            alert("Usted necesita una linea de 500 grains");
+            alert("Le gusta ir a lo grande ;-)");
+            alert("Ven a disfrutar a https://proyecto-outdoor.vercel.app/");
+            break;
+        case 15:
+            alert("Usted necesita una linea de 550 grains");
+            alert("Le gusta ir a lo grande ;-)");
+            alert("Ven a disfrutar a https://proyecto-outdoor.vercel.app/");
+            break;
+        default:
+            alert("Desea aprender? ingrese a https://proyecto-outdoor.vercel.app/secciones/actividades o dejenos su e-mail a continuacion que nosotros lo contactamos");
+            parent(prompt("Ingrese su e-mail para enseñarle mas sobre Flyfishing"));
+            break;
+    }
+    numeroCaña = parseInt(prompt("Ingrese el numero de su caña (1-15)"));
+
+}
